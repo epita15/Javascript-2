dbf685894335860dd9422de3aae6343d8e4a5dad
epita15 <valeri.epitashvili241@eab.tsu.edu.ge>
2022-10-21 14:35:06 +0400

    javascript davaleba 2

---
+++ javascript.js
@@ -0,0 +1,74 @@
+1
+
+// function sum(...numbers){
+//     let numbersum = 0;
+//     for (let item of numbers) {
+//         if (item > 0)
+//         numbersum = numbersum + item;
+//     }
+//     console.log(numbersum);
+
+// }
+// sum(2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8)
+
+
+2
+
+// function sum(...number){
+//     let numbersum = 0
+//     for (let item of number){
+//         numbersum = numbersum + item;
+//     }
+//     console.log(numbersum);
+
+// }
+// sum(10, 50, 6, 7, 8, 11, 6, 3, 9)
+
+
+3
+
+
+// let user = {
+//     firstname: 'giorgi',
+//     lastname: 'saakadze',
+//     age: 32,
+//     isloggedin: true
+//   };
+//   let printname = user.firstname + " " + user.lastname;
+// //   let printname = " ${user.firstname}  ${user.lastname} ";
+
+//   console.log(printname);
+
+4
+
+
+5
+
+
+// let array = [1,2,4,10,34,5,7,87]
+// for(let items of array){
+//     if(items > 0 && items < 10){
+//         console.log(items);
+//     }
+// }
+
+6
+
+// let numbers =[1,2,3,4,5,6,7,8,9,10]
+// for(let item of numbers){
+//     console.log(item);
+//     if(item == 5){
+//         break;
+//     }
+// }
+
+
+7
+
+
+// let array = ["html", "css", "python", "javascript", "bootstrap"];
+// for(let items of array){
+//     if(items.length > 4 && items.includes("av")){
+//         console.log(items);
+//     }
+// }
